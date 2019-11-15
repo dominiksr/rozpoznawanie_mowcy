596e053de0cbc8d5d7f4181af4870719fe8b329a
JoanMierz <48381284+JoanMierz@users.noreply.github.com>
2019-11-15 15:59:31 +0100

    Dodanie pliku LBG.py

---
+++ LBG.py
@@ -0,0 +1,68 @@
+# Wektor kwantyzacji używając metody LBG. Tworzy codebook
+# odpowiednio grupując dane.
+import numpy as np
+
+
+# Obliczanie odległości euklidesowej pomiędzy euklidesową dwiema macierzami.
+def EUDistance(d, c):
+
+    # Tworzenie zmiennych przechowywujących wymiar macierzy.
+    n = np.shape(d)[1]
+    p = np.shape(c)[1]
+    distance = np.empty((n, p))
+    if n < p:
+        for i in range(n):
+            copies = np.transpose(np.tile(d[:, i], (p, 1)))
+            distance[i, :] = np.sum((copies - c) ** 2, 0)
+    else:
+        for i in range(p):
+            copies = np.transpose(np.tile(c[:, i], (n, 1)))
+            distance[:, i] = np.transpose(np.sum((d - copies) ** 2, 0))
+    distance = np.sqrt(distance)
+
+    return distance
+
+
+# Metoda LBG.
+def lbg(cechy, M):
+    eps = 0.01
+    codebook = np.mean(cechy, 1)
+    distortion = 1
+    nCentroid = 1
+    while nCentroid < M:
+
+        # Podwajanie wielkości codebook'a.
+        nowy_codebook = np.empty((len(codebook), nCentroid * 2))
+        if nCentroid == 1:
+            nowy_codebook[:, 0] = codebook * (1 + eps)
+            nowy_codebook[:, 1] = codebook * (1 - eps)
+        else:
+            for i in range(nCentroid):
+                nowy_codebook[:, 2 * i] = codebook[:, i] * (1 + eps)
+                nowy_codebook[:, 2 * i + 1] = codebook[:, i] * (1 - eps)
+
+        codebook = nowy_codebook
+        nCentroid = np.shape(codebook)[1]
+        D = EUDistance(cechy, codebook)
+
+        while np.abs(distortion) > eps:
+
+            # Wyszukiwanie najbliższego sąsiada.
+            poprzedni_distance = np.mean(D)
+            najblizszy_codebook = np.argmin(D, axis=1)
+
+            # Grupowanie wektorów i szukanie nowego centroidu.
+            for i in range(nCentroid):
+
+                # Dodawanie następuje w 3 wymiarze.
+                codebook[:, i] = np.mean(
+                    cechy[:, np.where(najblizszy_codebook == i)], 2
+                ).T
+
+            # replace all NaN values with 0
+            codebook = np.nan_to_num(codebook)
+            # print 'this codebook', codebook
+            D = EUDistance(cechy, codebook)
+            distortion = (poprzedni_distance - np.mean(D)) / poprzedni_distance
+
+    return codebook
